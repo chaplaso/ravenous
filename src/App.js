ec23aafd96aa6cd72f29651d3d8767e81bcb2c1b
Chaplaso-Ola <chaplaso@yahoo.com>
2019-03-31 15:05:16 -0400

    completed stg1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SearchBar from './components/searchBar/searchBar'
+import BusinessList from './components/businessList/businessList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+          <SearchBar />
+          <BusinessList />
+      </div>
+    );
+  }
+}
+
+export default App;
